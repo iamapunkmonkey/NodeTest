c8d35ae5cbde985993cba8f465c97b745ad8c332
Zachary Breman <zbreman@gmail.com>
2011-04-10 17:30:55 -0400

    Think I got it this time.

--- hello.js
+++ hello.js
@@ -1,10 +1,12 @@
 var sys = require("sys"),
     http = require("http");
 
-http.createServer(function(request, response) {
-    response.writeHeader(200, {"Content-Type": "text/html"});
-    response.write("Hello World!");
-    response.close();
+http.createServer(function(req, res) {
+	setTimeout(function(){
+		res.writeHeader(200, {"Content-Type": "text/html"});
+		res.write("Hello World!");
+		res.close();
+	}, 2000);
 }).listen(8080);
 
 sys.puts("Server running on port 8080");
