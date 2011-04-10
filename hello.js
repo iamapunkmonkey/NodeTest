e46aeb9fed1ff47f61afd90e2f21746453772f11
Zachary Breman <zbreman@gmail.com>
2011-04-10 17:26:17 -0400

    Hello World part 3

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var sys = require("sys"),
     http = require("http");
 
 http.createServer(function(request, response) {
-    response.sendHeader(200, {"Content-Type": "text/html"});
+    response.writeHeader(200, {"Content-Type": "text/html"});
     response.write("Hello World!");
     response.close();
 }).listen(8080);
