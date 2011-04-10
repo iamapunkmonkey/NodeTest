bf558d5c3bfa61541c69f2f3eb27c50ba8777416
Zachary Breman <zbreman@gmail.com>
2011-04-10 17:37:09 -0400

    getting into sockets now.

--- hello.js
+++ hello.js
@@ -1,12 +1,14 @@
-var sys = require("sys"),
-    http = require("http");
+http = require('http');
+net = require('net');
+c = 0;
 
-http.createServer(function(req, res) {
-	setTimeout(function(){
-		res.writeHeader(200, {"Content-Type": "text/html"});
-		res.write("Hello World!");
-		res.close();
-	}, 2000);
+http.createServer(function(req, res){
+	c++;
+	res.writeHead(200);
+	res.end('hello world\n');
 }).listen(8080);
 
-sys.puts("Server running on port 8080");
+net.createServer(function(socket){
+	socket.write('connections: ' + c);
+	socket.end();
+}).listen(8081);
