df9dee2920aa9826733b306d8ab96be7b893b9f2
Zachary Breman <zbreman@gmail.com>
2011-04-10 17:22:40 -0400

    Hello World part 2

--- hello.js
+++ hello.js
@@ -1,10 +1,10 @@
 var sys = require("sys"),
-	http = require("http");
-	
-http.createServer(function(request, response){
-	response.sendHeader(200, {"Content-Type": "text/html"});
-	response.write("Hello World");
-	response.close();
+    http = require("http");
+
+http.createServer(function(request, response) {
+    response.sendHeader(200, {"Content-Type": "text/html"});
+    response.write("Hello World!");
+    response.close();
 }).listen(8080);
 
 sys.puts("Server running on port 8080");
