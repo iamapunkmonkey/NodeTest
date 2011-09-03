29b15c856fedacc9e478fc220f2fc9ad0359a92c
Zach Breman <zbreman@gmail.com>
2011-09-02 20:18:30 -0400

    Adding in the server

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var http = require("http");
 
-function onRequest(request, response){
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+function start(){
+	function onRequest(request, response){
+		console.log("Request received.");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
+	
+	http.createServer(onRequest).listen(8888);
+	
+	console.log("Server has Started");
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log("Server has Started");
+exports.start = start;
